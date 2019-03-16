cac2a438289bc92e87ac26043c9344340af4601c
karol <carolina.chamorro@kairosds.com>
2019-03-16 15:22:34 +0100

    estilos componente home

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+// import scss
   export default {
     name: 'App',
     data() {
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,4 +1,40 @@
-<template src="./home.html"></template>
-<script src="./home.js"></script>
-<style src="./home.scss" scoped lang="scss"></style>
+<template>
+  <section class="home">
+    <div class="border"></div>
+    <h1>{{ logo }}</h1>
+    <project></project>
+    <project></project>
+    <project></project>
+    <button class="addNewProject">{{ add }}</button>
+  </section>
+</template>
+
+<script>
+  import project from '@/components/view-parts/project'
+  export default {
+    name: 'home',
+    components: {
+      'project': project
+    },
+    props: [],
+    data () {
+      return {
+        logo: 'Type Something',
+        add: 'Añadir nuevo'
+      }
+    },
+    computed: {
+
+    },
+    mounted () {
+
+    },
+    methods: {
+
+    }
+  }
+
+</script>
+<style src="./home.scss" scoped lang="scss">
+</style>
 
