4030fe963fb7a256fb2bd0706f66b3525b568b53
Lucas Bernalte <lucas.bernalte.contractor@bbva.com>
2019-03-18 20:19:04 +0100

    removed filtering projects

--- src/containers/projects/index.vue
+++ src/containers/projects/index.vue
@@ -2,44 +2,40 @@
   <section class="home">
     <div class="border"></div>
     <h1>{{ logo }}</h1>
-    <input class="search" type="text" v-model="inputText" placeholder="search...">
-    <div v-if="showProjects">
-      <section class="project">
-        <div class="image"></div>
-        <div class="textContent">
-          <div class="header-box">
-            <h4 class="title">{{ projects[0].title }}</h4>
-            <div class="btns">
-              <span>
-                <i class="fas fa-times"></i>
-              </span>
-              <span>
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </div>
+    <section class="project">
+      <div class="image"></div>
+      <div class="textContent">
+        <div class="header-box">
+          <h4 class="title">{{ projects[0].title }}</h4>
+          <div class="btns">
+            <span>
+              <i class="fas fa-times"></i>
+            </span>
+            <span>
+              <i class="fas fa-pencil-alt"></i>
+            </span>
           </div>
-          <p class="text">{{ projects[0].text }}</p>
         </div>
-      </section>
-      <section class="project">
-        <div class="image"></div>
-        <div class="textContent">
-          <div class="header-box">
-            <h4 class="title">{{ projects[1].title }}</h4>
-            <div class="btns">
-              <span>
-                <i class="fas fa-times"></i>
-              </span>
-              <span>
-                <i class="fas fa-pencil-alt"></i>
-              </span>
-            </div>
+        <p class="text">{{ projects[0].text }}</p>
+      </div>
+    </section>
+    <section class="project">
+      <div class="image"></div>
+      <div class="textContent">
+        <div class="header-box">
+          <h4 class="title">{{ projects[1].title }}</h4>
+          <div class="btns">
+            <span>
+              <i class="fas fa-times"></i>
+            </span>
+            <span>
+              <i class="fas fa-pencil-alt"></i>
+            </span>
           </div>
-          <p class="text">{{ projects[1].text }}</p>
         </div>
-      </section>
-    </div>
-    <div v-if="!showProjects">No projects found</div>
+        <p class="text">{{ projects[1].text }}</p>
+      </div>
+    </section>
   </section>
 </template>
 
@@ -60,19 +56,9 @@ export default {
         text: "descripcion 2"
       }
     ],
-    inputText: "",
     logo: "My projects"
   }),
-  computed: {
-    filteredProjects: function() {
-      return this.projects.filter(
-        p => p.title.toLowerCase().indexOf(this.inputText.toLowerCase()) > -1
-      );
-    },
-    showProjects: function() {
-      return this.filteredProjects.length !== 0;
-    }
-  },
+  computed: {},
   methods: {}
 };
 </script>
