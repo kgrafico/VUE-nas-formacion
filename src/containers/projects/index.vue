878d5fea4d5bd06c6dac04880de306128109d5e3
Lucas Bernalte <lucas.bernalte.contractor@bbva.com>
2019-03-18 20:22:34 +0100

    removed data binding

--- src/containers/projects/index.vue
+++ src/containers/projects/index.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="home">
     <div class="border"></div>
-    <h1>{{ logo }}</h1>
+    <h1>My projects</h1>
     <section class="project">
       <div class="image"></div>
       <div class="textContent">
         <div class="header-box">
-          <h4 class="title">{{ projects[0].title }}</h4>
+          <h4 class="title">Título</h4>
           <div class="btns">
             <span>
               <i class="fas fa-times"></i>
@@ -16,24 +16,7 @@
             </span>
           </div>
         </div>
-        <p class="text">{{ projects[0].text }}</p>
-      </div>
-    </section>
-    <section class="project">
-      <div class="image"></div>
-      <div class="textContent">
-        <div class="header-box">
-          <h4 class="title">{{ projects[1].title }}</h4>
-          <div class="btns">
-            <span>
-              <i class="fas fa-times"></i>
-            </span>
-            <span>
-              <i class="fas fa-pencil-alt"></i>
-            </span>
-          </div>
-        </div>
-        <p class="text">{{ projects[1].text }}</p>
+        <p class="text">texto de ejemplo</p>
       </div>
     </section>
   </section>
@@ -44,19 +27,6 @@ export default {
   name: "projects",
   props: [],
   data: () => ({
-    projects: [
-      {
-        id: 1,
-        title: "PROYECTO 1",
-        text: "descripcion 1"
-      },
-      {
-        id: 2,
-        title: "PROYECTO 2",
-        text: "descripcion 2"
-      }
-    ],
-    logo: "My projects"
   }),
   computed: {},
   methods: {}
